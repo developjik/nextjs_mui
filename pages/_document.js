c0fb8c15abbdd39facc5fee91d6ec55e106a0d71
developjik <developjik@gmail.com>
2022-06-02 21:53:34 +0900

    feature: 기본react-table 구현

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import "regenerator-runtime/runtime";
+
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
