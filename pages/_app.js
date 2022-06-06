19dcf4b1e6df4209012b87df337328e78e6eb024
developjik <developjik@gmail.com>
2022-06-07 01:53:52 +0900

    feat: 프로젝트 기본 설정 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@material-ui/core";
+import theme from "styles/theme";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <MuiThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </MuiThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
